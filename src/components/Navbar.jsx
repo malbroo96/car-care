5f603ebf700f72b114cd80ae9bca50ba94453358
malbroo96 <akhiljoseph225292@gmail.com>
2026-01-10 13:23:22 +0530

    renamed to navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+export default function Navbar({
+  categories,
+  activeCategory,
+  setActiveCategory
+}) {
+
+  return (
+    <nav className="sticky top-0 bg-white z-50 border-b">
+      <div className="max-w-6xl mx-auto px-4 py-3">
+        <div
+          className="
+            grid gap-3
+            grid-cols-2
+            sm:grid-cols-3
+            md:grid-cols-4
+            lg:grid-cols-7
+          "
+        >
+          {categories.map(category => (
+            <button
+              key={category}
+              onClick={() => setActiveCategory(category)}
+              className={`py-2 text-sm rounded-full border transition text-center
+                ${
+                  activeCategory === category
+                    ? "bg-black text-white"
+                    : "bg-white text-black"
+                }`}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+}
