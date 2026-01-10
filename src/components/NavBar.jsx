4d7504931786bd03e40ba94fd091832ab3f4e2d2
malbroo96 <akhiljoseph225292@gmail.com>
2026-01-10 13:13:50 +0530

    updated the file name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ export default function Navbar({
   activeCategory,
   setActiveCategory
 }) {
+  console.log(name)
   return (
     <nav className="sticky top-0 bg-white z-50 border-b">
       <div className="max-w-6xl mx-auto px-4 py-3">
